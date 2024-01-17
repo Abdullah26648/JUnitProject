58bf456fb8e29e46384221e4cfef42722e21b023
Abdullah26648 <abdullah26648@gmail.com>
2024-01-17 17:13:01 +0300

    17.01.2024

---
+++ src/test/java/Tasks/Task_06.java
@@ -0,0 +1,58 @@
+package Tasks;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Task_06 {
+    /*
+    Go to http://test.rubywatir.com/ifelse.php
+    Click the correct submit button declared in the yellow line
+    Finish the test after 50 correct click
+    */
+    private WebDriver driver;
+    @Before
+    public void setup() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+    }
+
+    @After
+    public void tearDown() throws InterruptedException {
+
+        Thread.sleep(3000);
+        driver.quit();
+
+    }
+
+    @Test
+    public void test() {
+        // Go to http://test.rubywatir.com/ifelse.php
+        driver.get("http://test.rubywatir.com/ifelse.php");
+
+        // Finish the test after 50 correct clicks
+        for (int i = 0; i < 50; i++) {
+
+            // Get the displayed number
+            String displayedNumber = driver.findElement(By.xpath("/html/body/div/div[3]/div/div/div[2]/div[1]/h2")).getText();
+
+            // Find and click the submit button based on the displayed number using switch
+            WebElement submitButton = driver.findElement(By.xpath("//input[contains(@name,"+displayedNumber+")]"));
+            submitButton.click();
+
+            // Wait to adjust for the page behavior
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        }
+    }
+
+}
